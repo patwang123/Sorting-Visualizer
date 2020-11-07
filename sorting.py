75cf133d5dc5c846fa4b0199da7087b5c030fd23
patwang123 <52119109+patwang123@users.noreply.github.com>
2020-11-07 16:08:27 -0500

    sorting preliminaries
    added in quick sort/insertion sort/visualizer

---
+++ sorting.py
@@ -0,0 +1,62 @@
+import random
+import time
+
+class Algorithm:
+	def __init__(self,size,name):
+		self.name = name
+		self.size = size
+		self.arr = [1+x for x in random.sample(range(size),size)]
+		self.color = ['b' for b in self.arr]
+		self.swaps = 0
+		self.comparisons = 0
+	def start(self):
+		self.time = time.time()
+		yield from self.sort()
+class QuickSort(Algorithm):
+	def __init__(self,size):
+		super().__init__(size,'Quick Sort')
+	def sort(self,p=0,q=-100):
+		if q == -100:
+			yield from self.sort(0,self.size-1)
+			return
+		self.comparisons += 1
+		if p == q:
+			yield [[p,'g']]
+		if p >= q:
+			return
+		piv = self.arr[q]
+		idx = p
+		for i in range(p,q):
+			self.comparisons += 1
+			if self.arr[i] < piv:
+				self.swaps += 1
+				self.arr[i],self.arr[idx] = self.arr[idx],self.arr[i]
+				yield [[i,self.arr[i]],[idx,self.arr[idx]]]
+				idx += 1
+		self.swaps += 1
+		self.arr[q],self.arr[idx] = self.arr[idx],self.arr[q]
+		yield [[q,self.arr[q]],[idx,self.arr[idx]],[idx,'g']]
+		yield from self.sort(p,idx-1)
+		yield from self.sort(idx+1,q)
+
+class InsertionSort(Algorithm):
+	def __init__(self,size):
+		super().__init__(size,'Insertion Sort')
+	def sort(self):
+		for i in range(1,len(self.arr)):
+			piv = self.arr[i]
+			j = i-1
+			while j >= 0 and self.arr[j] > piv:
+				self.comparisons += 2
+				self.arr[j+1] = self.arr[j]
+				self.swaps += 1
+				yield [[j+1,self.arr[j+1]]]
+				j -= 1
+			self.arr[j+1] = piv
+			self.swaps += 1
+			yield [[j+1,piv]]
+		for i in range(len(self.arr)):
+			yield [[i,'g']]
+class MergeSort(Algorithm):
+	def arr_sort(self):
+		pass
---
+++ visualizing.py
@@ -0,0 +1,36 @@
+import random
+import time
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+from sorting import *
+
+def main():
+	n = int(input("Size of array: "))
+	al = int(input("Choose sorting algorithm (0: QuickSort, 1: MergeSort, 2: InsertionSort): "))
+
+	sort_dict = {0: QuickSort, 1: MergeSort, 2: InsertionSort}
+	sorter = sort_dict[al](n)
+	frames = sorter.start()
+
+	fig, ax = plt.subplots()
+	ax.set_title(sorter.name)
+
+	bar = ax.bar(range(len(sorter.arr)), sorter.arr, align='edge')
+
+	ax.set_xlim(0, n)
+	ax.set_ylim(0, n)
+	text = ax.set_xlabel('Swaps: 0')
+
+	def animate(frames, bar=bar):
+		for f in frames:
+			if type(f[1]) == str:
+				bar[f[0]].set_color(f[1])
+			else:
+				bar[f[0]].set_height(f[1])
+				text.set_text("Swaps: {0}, Comparisons: {1}, Time: {2} seconds".format(\
+					sorter.swaps,sorter.comparisons,"%.2f" % round(time.time()-sorter.time,2)))
+
+	animation = anim.FuncAnimation(fig, func=animate, frames=frames, interval=5, repeat=False,cache_frame_data=False)
+	plt.show()
+if __name__ == "__main__":
+    main()
